49408d32d237053ced115e423240abf0ae111eeb
Raja CSP Raman <raja.raman@syndigo.com>
2024-01-28 11:05:58 +0530

    I'm not saying it's your fault, but it can't be mine either.: Updates for 2024-01-28

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 
+'''
+Created on 
 
+@author: Raja CSP Raman
+
+source:
+
+    Salesforce blip image captioning base
+    https://huggingface.co/Salesforce/blip-image-captioning-base
+'''
+
+def startpy():
+
+    print("Tact101")
+    
+
+if __name__ == '__main__':
+    startpy()
 
 import streamlit as st
 from dotenv import find_dotenv, load_dotenv
 from transformers import pipeline
-import requests
-import os
+
+# Local
+import custom
+
+
 # from transformers import AutoTokenizer, AutoModelForCausalLM
-from IPython.display import Audio
 
 # st.title("Image to Audio Text Generation")
 
 load_dotenv(find_dotenv())
-HUGGINGFACEHUB_API_TOKENS = os.getenv("api_token")
+
 
 # print(f'HUGGINGFACEHUB_API_TOKENS : {HUGGINGFACEHUB_API_TOKENS}')
 
@@ -26,39 +45,6 @@ def img2text(url):
     st.image(url, use_column_width = True)
     return text
 
-
-def get_story():
-
-    API_URL         = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
-    headers         = {"Authorization": "Bearer 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"}
-
-    def query(prompt, max_new_tokens = 200):
-        payload = {
-            "inputs": prompt,
-            "parameters": {
-                "max_new_tokens": max_new_tokens
-            }
-        }
-        response = requests.post(API_URL, headers = headers, json = payload)
-        return response.json()
-
-    data            = query(image_caption, max_new_tokens = 250)
-    generated_text  = data[0].get("generated_text", "")
-    st.text("Generated Text:")
-    st.write(generated_text)
-
-    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-    headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKENS}"}
-
-    def generate_and_play_audio(text, sampling_rate = 22050):
-        payload     = {"inputs": text}
-        response    = requests.post(API_URL, headers = headers, json = payload)
-        audio       = response.content
-
-        # Create an Audio object to play the binary audio data
-        return Audio(audio, rate = sampling_rate)
-
-
 # Get the image URL from the user
 image_url = st.text_input("Enter the URL of the image:")
 if st.button("Generate Text from Image"):
@@ -67,4 +53,4 @@ if st.button("Generate Text from Image"):
 
     print(f'image_caption : {image_caption}')
     
-    # get_story()
+    custom.get_story(image_caption, st)
---
+++ custom.py
@@ -0,0 +1,38 @@
+
+
+import os
+import requests
+from IPython.display import Audio
+
+HUGGINGFACEHUB_API_TOKENS = os.getenv("api_token")
+
+def get_story(image_caption, st):
+
+    API_URL         = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
+    headers         = {"Authorization": "Bearer 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"}
+
+    def query(prompt, max_new_tokens = 200):
+        payload = {
+            "inputs": prompt,
+            "parameters": {
+                "max_new_tokens": max_new_tokens
+            }
+        }
+        response = requests.post(API_URL, headers = headers, json = payload)
+        return response.json()
+
+    data            = query(image_caption, max_new_tokens = 250)
+    generated_text  = data[0].get("generated_text", "")
+    st.text("Generated Text:")
+    st.write(generated_text)
+
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKENS}"}
+
+    def generate_and_play_audio(text, sampling_rate = 22050):
+        payload     = {"inputs": text}
+        response    = requests.post(API_URL, headers = headers, json = payload)
+        audio       = response.content
+
+        # Create an Audio object to play the binary audio data
+        return Audio(audio, rate = sampling_rate)
